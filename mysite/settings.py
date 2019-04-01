8ff37c41dfc43027fc3aab9af478eda353535a2f
Katarzyna Kolasa <kt.kolasa@gmail.com>
2019-04-01 20:21:13 +0200

    adjust config to whitenoise v4.0

--- mysite/settings.py
+++ mysite/settings.py
@@ -99,6 +99,8 @@ STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
