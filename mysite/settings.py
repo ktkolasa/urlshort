9aac6ea0b4f7e1f5f6730fd0e690ac136bcce7e2
Katarzyna Kolasa <kt.kolasa@gmail.com>
2019-04-01 20:11:43 +0200

    adjust config to whitenoise v4.0

--- mysite/settings.py
+++ mysite/settings.py
@@ -103,4 +103,4 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
