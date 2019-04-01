a6d53b1d835a446b5ee9d796d9998537455a12be
Katarzyna Kolasa <kt.kolasa@gmail.com>
2019-04-01 19:54:17 +0200

    add files for deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,8 +92,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
